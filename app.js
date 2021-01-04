3f9202716944a6d8bce18cdaf131212c66e9e0b3
McNobby <76711087+McNobby@users.noreply.github.com>
2021-01-04 18:10:12 +0100

    Realease 1

---
+++ app.js
@@ -0,0 +1,111 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const guild = new Discord.Guild()
+const { token, prefix, swears, logChannel } = require('./config.json')
+const commands = require('./commands.js')
+
+//connect 
+client.on('ready', () => {
+    console.log("connected as " + client.user.tag);
+    client.user.setActivity("With logs", {type: 'PLAYING'})
+ 
+})
+
+client.login(token)
+
+//message listener
+client.on('message', (recievedMessage) => {
+    //ignores messages from dm's
+    if (recievedMessage.channel.type == "dm"){
+        return
+    }
+    
+    //checks if message author is bot
+    if (recievedMessage.author == client.user) {
+        return
+    }
+
+//logs recieved message
+    console.log(`Mesaage recieved: "${recievedMessage.content}" from: ${recievedMessage.author.username}`); 
+
+    //checks if message contains the prefix
+    if (recievedMessage.content.startsWith(prefix)){
+        processCommand(recievedMessage)
+
+    }
+
+    //profanity filter
+    //checks if any message contains any word from the swear array in config.json
+    //makes the input lowercase (.lowercase())
+    //also removes all spaces with (.replace(/\s+/g)) to detect slurs written like this: S L U R
+        //\s+ is all whitespaces, /g is global. so it replaces all whitespace in global with '', in other words nothing
+
+if(swears.some(word => recievedMessage.content.toLowerCase().replace(/\s+/g, '').includes(word))){
+        
+    //sends a warning that slurs are not tolerated, and deleted the message containing the slur
+        recievedMessage.channel.send("Slurs are not tolerated <@" + recievedMessage.author.id + ">")
+        recievedMessage.delete()
+        .catch(console.error);
+
+        //gives the offending member the role named Muted
+        let role = recievedMessage.guild.roles.cache.find(r => r.name === "Muted");
+        let member = recievedMessage.member
+        //mutedroleid for test server 793074569433972736
+        member.roles.add(role);
+        
+        //mod log channel name
+        const CHANNEL = 'mod-logs';
+        //finds log channel
+        if (recievedMessage.channel.type == 'text') {
+            var logger = recievedMessage.guild.channels.cache.find(
+                channel => channel.name === logChannel
+            );
+            //sends slur log embed
+            if (logger) { 
+                   //the embed
+                const embed = new Discord.MessageEmbed()
+                .setTitle(`Slur Usage Detected!`)
+                .addField('Offending member;', '<@' + recievedMessage.author.id + '>')
+                .addField('Offending message;', recievedMessage.cleanContent)
+                .addField('Action taken; ', 'Member Muted. Staff, please unmute using the unmute when you see fit.')
+                .addField('React to this message when you unmute the offender!', "Or we will get angry :D")
+                .addField('False trigger or something wrong?', 'Contact my developers @Mc_nobby#6969 or @Jaack#7159 with a screenshot')
+                .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+                .setColor('#b8002e');
+                //send in log channel
+                logger.send({ embed });
+                recievedMessage.author.send(muteDM)
+                //makes sure the the bot dosen't 
+                .catch(() => console.log("Can't send DM to your user!"))
+            }
+            
+        }
+    }
+
+
+});
+
+
+
+//processes the promted command, and checks if its valid Jeg har ingen will to live
+function processCommand(recievedMessage){
+    let fullCommand = recievedMessage.content.substr(1)
+    let splitCommand = fullCommand.split(" ")
+    let primaryCommand = splitCommand[0]
+    let arguments = splitCommand.slice(1)
+
+    console.log(`primary command: "${primaryCommand}" from ${recievedMessage.author.username}`);
+    
+    //hands off user input the the command.js command function
+    commands.command(recievedMessage, primaryCommand, arguments)
+}
+
+//vv embeds vv
+const muteDM = new Discord.MessageEmbed()
+    .setTitle("You have been muted!")
+    .addField("This means:", "That you've broken a rule, or that we've deemed your message inappropiate.")
+    .addField("Try to mute evade?","If you mute evade by rejoining the server you will be banned permanently!")
+    .addField("False trigger?", "If you believe that this is a false trigger you will have to wait 30 minutes and see if you are unmuted by one of our staff. If you are still muted after that time, go ahead and make a ticket appealing your mute (keep in mind, we have the logs)")
+    .setColor("#b8002e")
+    .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+    .setImage("https://i.imgur.com/48H0ILI.png")
---
+++ commands.js
@@ -0,0 +1,306 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const app = require('./app.js')
+const { logChannel, mutedRole, staffRole, adminRole } = require('./config.json')
+const reason = null
+
+
+//command recognition function
+//to add a new command, just add a new else if statement
+//example:
+//else if (primaryCommand == "newCommand (without the prefix) "){
+//  action    
+//}
+module.exports.command = (recievedMessage, primaryCommand, arguments) => {
+
+    let pCmd = primaryCommand.toLowerCase();
+    //let lowercaseArgs = arguments(0).toLowerCase(); -- not tested but should work in theory
+
+    //test command
+    if (pCmd == "test") {
+        if (arguments == "argument") {
+            recievedMessage.channel.send("you have been tested")
+        }
+        else if (arguments == "ayo") {
+            recievedMessage.channel.send("Ayo it was tested")
+        }
+    }
+    
+    else if (pCmd == "intro"){
+        if (recievedMessage.member.roles.cache.find(r => r.name === adminRole)){
+            recievedMessage.channel.send(staffhello)
+            recievedMessage.delete()
+        }
+
+    }
+
+     // disabled for v1 as there is no command for non staff
+    //provides a list of commands for the normies (!help)
+   // else if (pCmd == "help") {
+     
+    //   recievedMessage.channel.send(helpEmbed)
+    //}
+
+
+    //provides a list of staff commands (!modhelp)
+    else if (pCmd == 'staffhelp') {
+        //checks if author is staff
+        if (recievedMessage.member.roles.cache.find(r => r.name === staffRole)){
+            recievedMessage.channel.send(staffhelpembed)
+        }
+        else{
+            //sends DM if the author isnt a staff member
+            console.log(`${recievedMessage.author.username} is not a staff member(tried to use modhelp)`);
+            recievedMessage.delete()
+            recievedMessage.author.send(`${recievedMessage.author.toString()} You don't have access to that command!`)
+            
+        }
+    }
+
+
+    // unmute command -- its smart to collapse this if possible to save space on the screen
+    else if (pCmd == 'unmute') {
+        if (recievedMessage.member.roles.cache.find(r => r.name === staffRole)){
+            //checks if there is any arguments
+            if(arguments != ''){
+            //parameters to check if there is a valid ping
+            const mention = arguments[0].match(/^<@!?(\d+)>$/)
+                //checks if there is a valid ping
+                if (mention){
+                    let role = recievedMessage.guild.roles.cache.find(r => r.name === mutedRole);
+                    let member = recievedMessage.mentions.members.first();
+                  
+                    //checks if the pinged member is muted
+                    if (member.roles.cache.some(rolee => rolee.name === mutedRole)){
+                        //removes the muted role of the pinged  and deletes the prompted command
+                        member.roles.remove(role);
+                        recievedMessage.delete()
+                        
+                        //finds the log channel from the channel name in config
+                        if (recievedMessage.channel.type == 'text') {
+                            var logger = recievedMessage.guild.channels.cache.find(
+                                channel => channel.name === logChannel
+                        );
+                            if (logger) { 
+                                const unmuteEmbed = new Discord.MessageEmbed()
+                                .setTitle('Member Unmuted!')
+                                .addField('Staff Responsible;', '<@' + recievedMessage.author.id + '>')
+                                .addField('Person Unmuted;', member)
+                                .addField('False trigger?', 'Contact my developers @Mc_nobby#6969 or @Jaack#7159')
+                                .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+                                .setColor('#03fca9');
+
+                                logger.send(unmuteEmbed);
+                                member.send(unMuteDM)
+                                //makes sure the the bot dosen't crash from sneding dm
+                                .catch(() => console.log("Can't send DM to your user!"))
+                            }
+                        }                             
+                    }
+                    else{
+                        console.log(`${recievedMessage.author.username} tried to unmute someone that isnt muted`);
+                        recievedMessage.delete()
+                        recievedMessage.author.send(`${recievedMessage.author.toString()} That member isnt muted! Someone might have already unmuted them, or they might have mute evaded!`)
+                    }
+                }  else{
+                    //sends DM if the argument isnt a valid ping
+                    console.log(`${recievedMessage.author.username} didnt provide a valid ping(unmute)`);
+                    recievedMessage.delete()
+                    recievedMessage.author.send(`${recievedMessage.author.toString()} You have to provide a valid ping, just like the one at the start of this sentence`)
+                }
+            }  else {
+                //sends DM if there is no arguments
+                console.log(`${recievedMessage.author.username} didnt supply any arguments(unmute)`);
+                recievedMessage.delete()
+                recievedMessage.author.send(`${recievedMessage.author.toString()} You have to have mention someone to unmute them!`)      
+            }
+        }
+        else{
+            //sends DM if the author isnt a staff member
+            console.log(`${recievedMessage.author.username} is not a staff member(tried to unmute)`);
+            recievedMessage.delete()
+            recievedMessage.author.send(`${recievedMessage.author.toString()} You don't have access to that command!`)
+
+        }
+        
+    }   
+
+    else if (pCmd == 'mute'){
+        if (recievedMessage.member.roles.cache.find(r => r.name === staffRole)){
+            //checks if there is any arguments
+            if(arguments != ''){
+            //parameters to check if there is a valid ping
+            const mention = arguments[0].match(/^<@!?(\d+)>$/)
+                //checks if there is a valid ping
+                if (mention){
+                    let role = recievedMessage.guild.roles.cache.find(r => r.name === mutedRole);
+                    let member = recievedMessage.mentions.members.first();
+                    
+                    //checks if the pinged member is muted
+                    if (!member.roles.cache.some(rolee => rolee.name === mutedRole)){
+                        member.roles.add(role);
+                        
+                        //WIP checks if there is a provided reason
+                        if (arguments[1]){
+                            if (arguments.slice(1)){
+                                //defines the reason
+                                let reasonArr = arguments.slice(1)
+                                let reason = reasonArr.join(" ")
+                                //let loud = reasonArr.match(/.+(-l)/)
+                                
+                                console.log(reasonArr);
+
+                                if (loud[1]){
+                                    console.log(reason);
+
+                                }
+                                
+                            }
+                        }
+                        
+                        
+                        //deletes message containing the promted command
+                        recievedMessage.delete()
+                        
+                        //finds the log channel based on name provided in config
+                        if (recievedMessage.channel.type == 'text') {
+                            var logger = recievedMessage.guild.channels.cache.find(
+                                channel => channel.name === logChannel
+                                
+                        );
+                            if (logger) { 
+                                
+                                const MuteEmbed = new Discord.MessageEmbed()
+                                .setTitle('Member Muted!')
+                                .addField('Staff Responsible;', '<@' + recievedMessage.author.id + '>')
+                                .addField('Person Muted;', member)
+                                .addField('Reason provided:', reason)
+                                .addField('False trigger?', 'Contact my developers @Mc_nobby#6969 or @Jaack#7159')
+                                .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+                                .setColor('#b8002e');
+                                
+                                //sends mute log embed
+                                logger.send(MuteEmbed);
+                                //sends a dm to the muted member saying they have been muted
+                                member.send(muteDM)
+                                //makes sure the the bot dosen't crash from sneding dm
+                                .catch(() => console.log("Can't send DM to your user!"))
+                            }
+                        }                             
+                    }
+                    else{
+                        //Sends dm to the staff member telling them theyre being a dum dum
+                        console.log(`${recievedMessage.author.username} tried to mute someone that is alrady muted`);
+                        recievedMessage.delete()
+                        recievedMessage.author.send(`${recievedMessage.author.toString()} That member is already muted`)
+                    }
+                }  else{
+                    //sends DM if the argument isnt a valid ping
+                    console.log(`${recievedMessage.author.username} didnt provide a valid ping(mute)`);
+                    recievedMessage.delete()
+                    recievedMessage.author.send(`${recievedMessage.author.toString()} You have to provide a valid ping, just like the one at the start of this sentence`)
+                }
+            }  else {
+                //sends DM if there is no arguments
+                console.log(`${recievedMessage.author.username} didnt supply any arguments(mute)`);
+                recievedMessage.delete()
+                recievedMessage.author.send(`${recievedMessage.author.toString()} You have to have mention someone to mute them!`)      
+            }
+        }
+        else{
+            //sends DM if the author isnt a staff member
+            console.log(`${recievedMessage.author.username} is not a staff member(tried to mute)`);
+            recievedMessage.delete()
+            recievedMessage.author.send(`${recievedMessage.author.toString()} You don't have access to that command!`)
+
+        }       
+
+        
+    }
+
+    else if (pCmd == 'allmuted'){
+        if (recievedMessage.member.roles.cache.find(r => r.name === adminRole)){
+            let allMembers = recievedMessage.guild.members
+            console.log(allMembers.cache.array.id)
+        }
+        
+    }
+
+
+    else if (pCmd == 'alexaoff') {
+        if (recievedMessage.member.roles.cache.find(r => r.name === adminRole)){
+            console.log("bye sisters");
+            client.role.find()
+        }
+
+    }
+        
+   
+
+    
+
+    //enter new commands above this line
+    //if a command is not registered the bot returns this. MUST be the last statement of the function to work
+    else {
+        console.log("Sorry, I don't know that one! Try doing `!help` for more commands!");
+    }
+
+}
+
+
+
+
+
+
+//vvv embeds vvv
+
+//Controls what the help command looks like. To add a new field, do .addField. 
+const helpEmbed = new Discord.MessageEmbed()
+    .setTitle(`My commands list | My prefix is !`)  
+    .addField("``hello world``", "hello World")
+    .addField("Got any suggestions? Need a bug fixing? Contact my developers, @Jaack#7159 and @Mc_nobby#6969")
+    //gif - .setImage("https://media.giphy.com/media/l0Ex3lGUCkofpN5UQ/giphy.gif")
+    .setImage("https://i.imgur.com/48H0ILI.png") //transparent logo
+    .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+    .setColor("#42e3b8")
+
+//mod help embed, to add new field do .addField
+const staffhelpembed = new Discord.MessageEmbed()
+    .setTitle("Hi, I'm AutoLogger, here to make your life easier. ")
+    .addField("!unmute", "This unmutes the user that you have tagged after the command. To tag a user who isn't in the channel you are in, do <@(their Id)>")
+    .addField("!mute", "This mutes the user that you have tagged after the command. ")
+    .addField("!allismuted", "Under maintenance. Teo and I are working on it as fast as we can D:")
+    .addField("!staffhelp", "The command you just used")
+    .addField("More questions? Want to tell us just how incredible our bot is? ", "Just talk to @Mc_nobby or @Jaack")
+    .setImage("https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif")
+    .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+    .setColor("#42e3b8")
+
+//embed for letting someone that was muted, know that they have been muted
+const muteDM = new Discord.MessageEmbed()
+    .setTitle("You have been muted!")
+    .addField("This means:", "That you've broken a rule, or that we've deemed your message inappropiate.")
+    .addField("Try to mute evade?","If you mute evade by rejoining the server you will be banned permanently!")
+    .addField("False trigger?", "If you believe that this is a false trigger you will have to wait 30 minutes and see if you are unmuted. If you are still muted after that time, go ahead and make a ticket appealing your mute (keep in mind, we have the logs)")
+    .setColor("#b8002e")
+    .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+    .setImage("https://i.imgur.com/48H0ILI.png")
+
+//embed for letting someone being unmuted that they are unmuted
+const unMuteDM = new Discord.MessageEmbed()
+    .setTitle("You have been unmuted!")
+    .addField("This means:", "That you can go ahead and chat as normal again!, but we'll be keeping an eye on you!")
+    .addField("If you get muted again:","You will be one step closer to being banned!")
+    .setColor("#03fca9")
+    .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+    .setImage("https://i.imgur.com/48H0ILI.png")
+
+
+const staffhello = new Discord.MessageEmbed()
+    .setTitle("Hello! I'm a new bot on the server. You may be wondering what I do!")
+    .addField("I'm a bot coded by Nobby and Jack/LaughnCry.", "This means that if you have any questions or suggestions about the bot, DM us!")
+    .addField("What is my purpose?", "My main purpose is to make logging and moderation easier for all staff. To see the commands I can do, type !staffhelp")
+    .addField("What if staff are asleep while people are spamming slurs?", "No worries! I automatically delete the message, and mute the user; I also log everyones mutes and unmutes for you guys!")
+    .setImage("https://media1.tenor.com/images/a7bd6b94430c1e66148d580209e377c5/tenor.gif?itemid=5043108")
+    .setThumbnail("https://i.imgur.com/IPNxl5W.png")
+    .setColor("#34ebdb")
