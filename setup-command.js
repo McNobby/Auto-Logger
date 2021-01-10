1128699c97899986f820b0a4cc36e80e14902dee
McNobby <76711087+McNobby@users.noreply.github.com>
2021-01-10 22:16:50 +0100

    Create setup-command.js

---
+++ setup-command.js
@@ -0,0 +1,95 @@
+const mongo = require('./mongo')
+const mongoose = require('mongoose')
+const setupSchema = require('./schemas/setup-schema')
+
+module.exports = async (arguments, guild, author, channel) => {
+    //!Logsetup <type> <channel/role>
+    
+    const types = ["actionlog", "deletionlog", "staffrole", "alog", "dlog", "srole"]
+    //validate arguments
+    if (!arguments[0]){
+        author.send('You need to supply arguments!')
+    }else{
+        if (!arguments[1]){
+            author.send('you need to supply a role or channel argument')
+        }else{
+            const type = arguments[0].toLowerCase()
+            const arg = arguments[1]
+
+            const typeRole = arg.match(/^<@&(\d+)>$/)
+            const typeChannel = arg.match(/^<#?(\d+)>$/)
+
+            if (types.includes(type)){
+                if(arg.match(/^<#?(\d+)>$/) || arg.match(/^<@&(\d+)>$/)){
+                    saveSetup(type, arg, guild, channel, typeChannel, typeRole)
+                    
+                }else{
+                    author.send("Invalid channel or role tag")
+                }
+            }else{
+                author.send("Invalid type, see `!setuphelp`")
+            }
+        }
+    }
+} 
+
+async function saveSetup(type, arg, guild, channel, typeChannel, typeRole){
+    if (type === "actionlog" || "alog") {
+        if (typeChannel){
+            await mongo().then(async mongoose => { 
+                try{
+                   await setupSchema.findOneAndUpdate({
+                        _id: guild.id
+                    },{
+                        _id: guild.id,
+                        actionLog: typeChannel[1],
+                    },{
+                        upsert: true
+                    })
+                }finally{
+                    mongoose.connection.close
+                }
+            })
+        }
+    }
+    else if (type === "deletionlog" || "dlog"){
+        if (typeChannel){
+            await mongo().then(async mongoose => { 
+                try{
+                   await setupSchema.findOneAndUpdate({
+                        _id: guild.id
+                    }, {
+                        _id: guild.id,
+                        deletionLog: typeChannel[1],
+                    },{
+                        upsert: true
+                    })
+                }finally{
+                    mongoose.connection.close
+                }
+            })
+        } 
+    }
+    else if (type === "staffrole" || "srole"){
+        if (typeRole){
+            await mongo().then(async mongoose => { 
+                try{
+                   await setupSchema.findOneAndUpdate({
+                        _id: guild.id
+                    },{
+                        _id: guild.id,
+                        staffRole: typeRole[1],
+                    },{
+                        upsert: true
+                    })
+                }finally{
+                    mongoose.connection.close
+                }
+            })
+        }
+    }
+}
+
+
+    
+
